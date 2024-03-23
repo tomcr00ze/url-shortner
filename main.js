7b01ce1b546f6e423752f5feca6794f81488f00d
tomcr00ze <tomcroose101@gmail.com>
2024-03-23 20:29:30 +0530

    downloaded dependencies for project setup.

---
+++ main.js
@@ -0,0 +1,11 @@
+// This is not the actual code. Just wrote it to check whether the setup is working or not.
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(8000, () => {
+  console.log(`Example app listening on port: 8000`)
+})
