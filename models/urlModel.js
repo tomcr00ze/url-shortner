bc5a772de36e2b61d5e5bbe707fc51e3715dfd41
tomcr00ze <tomcroose101@gmail.com>
2024-03-24 20:22:31 +0530

    added model and public directory with respected files. (no code only structure)

---
+++ models/urlModel.js
@@ -0,0 +1,2 @@
+// In this file we wikl be defining a URL schema with mongoose
+// this object will let us save the URL object to the MongoDB database and perform other queries.
---
+++ public/index.js
@@ -0,0 +1,1 @@
+// this file will contain frontend javascript code.
