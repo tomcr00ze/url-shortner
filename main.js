3995074c49cb25b2f0ee85574b1a22d6fce7b2be
tomcr00ze <tomcroose101@gmail.com>
2024-03-24 20:38:36 +0530

    added .env, URL Schema and updated main.js

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-// This is not the actual code. Just wrote it to check whether the setup is working or not.
-const express = require('express')
-const app = express()
+// We imported the libraries we installed earlier and some core modules from Nodejs.
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
+import urlExist from "url-exist";
+import URL from "./models/urlModel.js";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const __dirname = path.resolve();
+
+dotenv.config();
+
+// Initialized and created a new server with Express.
+const app = express();
+
+app.use(express.json());
+app.use(express.URLencoded({ extended: true }));
+
+// This line helps us server static files in the public folder.
+// Here we'll write our CSS and browser javascript code
+app.use(express.static(__dirname + "/public"));
 
 app.listen(8000, () => {
-  console.log(`Example app listening on port: 8000`)
-})
+  console.log("App listening on port 8000");
+});
--- models/urlModel.js
+++ models/urlModel.js
@@ -1,2 +1,18 @@
-// In this file we wikl be defining a URL schema with mongoose
+// In this file we will be defining a URL schema with mongoose
 // this object will let us save the URL object to the MongoDB database and perform other queries.
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+  url: {
+    required: true,
+    type: String,
+    },
+  id: {
+    required: true,
+    type: String
+    }
+});
+
+const URL = mongoose.model("URL", urlSchema);
+
+export default URL;
